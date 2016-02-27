3e50101db8fb7c1b4102ae832f606a9a54b1e282
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-02-28 06:08:56 +0900

    Add '/upload' routes
    `curl --upload-file FILE "/upload"`

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'digest/sha2'
 
 def filepath(filename)
   return "./objects/#{filename}"
@@ -16,5 +17,22 @@ get '/get/:filename' do |filename|
   else
     status 404
     "Not Found"
+  end
+end
+
+put '/upload' do
+  body = request.body.read
+  shasum = Digest::SHA2.hexdigest(body)
+  save_path = filepath(shasum)
+  if File.exist? save_path
+    return "Already exists"
+  end
+  begin
+    File.open(save_path, 'wb') do |file|
+      file.write body
+    end
+    "Successfully uploaded"
+  rescue
+    "Something wrong"
   end
 end
