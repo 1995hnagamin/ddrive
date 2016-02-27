f4574d2d0482921099f7386a6f23f540c6220fe4
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-02-28 03:44:09 +0900

    Add '/get/:filename' routes

--- server.rb
+++ server.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def filepath(filename)
+  return "./objects/#{filename}"
+end
+
 get '/' do
   'Hello World'
+end
+
+get '/get/:filename' do |filename|
+  path = filepath(filename)
+  if File.exist? path
+    send_file(path)
+  else
+    status 404
+    "Not Found"
+  end
 end
